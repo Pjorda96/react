1a99d915a0c9dbb52ac86351415cbc5cacc831b0
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-04-25 16:00:05 +0200

    citas react finished project

--- 03-citasReact/src/App.js
+++ 03-citasReact/src/App.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
+import Header from "./components/Header";
+import AgregarCita from "./components/AgregarCita";
+import ListaCitas from "./components/ListaCitas";
 
 class App extends Component {
+  state = {
+    citas: []
+  };
+
+  componentDidMount() {
+    const citasLS = localStorage.getItem('citas');
+
+    if (citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS)
+      })
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(
+      'citas',
+      JSON.stringify((this.state.citas))
+    )
+  }
+
+  crearCita = nuevaCita => {
+    const citas = [...this.state.citas, nuevaCita];
+
+    this.setState({
+      citas
+    })
+  };
+
+  borrarCita = id => {
+    const citasActuales = [...this.state.citas];
+
+    const citas = citasActuales.filter(cita => cita.id !== id);
+
+    this.setState({
+      citas
+    })
+  };
+
   render() {
     return (
-      <div className="App">
-
+      <div className="container">
+        <Header
+          titulo={'Administrador de Pacientes de Veterinaria'}
+        />
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita
+              crearCita={this.crearCita}
+            />
+          </div>
+          <div className="col-md-6">
+            <ListaCitas
+              citas={this.state.citas}
+              borrarCita={this.borrarCita}
+            />
+          </div>
+        </div>
       </div>
     );
   }
