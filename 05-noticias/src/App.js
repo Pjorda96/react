042483f6261b85f101ed5d0098384d3312106934
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-04-27 14:38:03 +0200

    noticias finished

--- 05-noticias/src/App.js
+++ 05-noticias/src/App.js
@@ -1,18 +1,47 @@
 import React, {Component} from 'react';
 import Header from "./components/Header";
+import Noticias from "./components/Noticias";
+import Formulario from "./components/Formulario";
 
-function App extends Component {
+class App extends Component {
+
+  state = {
+    noticias:[],
+  };
 
   componentDidMount() {
-
+    this.consultarNoticias();
   }
+
+  consultarNoticias = (category='general') => {
+    const key = 'f2c765f2dc5b4052bd46f73141f5e8b4';
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${key}`;
+
+    fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(noticias => {
+        this.setState({
+          noticias: noticias.articles,
+        })
+      })
+  };
 
   render() {
     return (
-      <div className="App">
+      <div className="contenedor-app">
         <Header
           titulo="Noticias"
         />
+        <div className="container white contenedor-noticias">
+          <Formulario
+            consultarNoticias={this.consultarNoticias}
+          />
+          <Noticias
+            noticias={this.state.noticias}
+          />
+        </div>
       </div>
     );
   }
